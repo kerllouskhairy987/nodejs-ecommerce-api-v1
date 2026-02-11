bce6535d135651c96bd82dce3e53150ae4c651d1
kerllouskhairy987 <kyrlskhyry80@gmail.com && git config --global user.name kerllouskhairy987git config --global user.email kyrlskhyry80@gmail.com>
2026-02-11 21:32:13 +0200

    error handling with express and apiError class

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const morgan = require("morgan");
 // files
 const dbConnection = require("./config/database");
 const categoryRoute = require("./router/categoryRoute");
+const apiError = require("./utils/apiError");
 
 dotenv.config({ path: "./config.env" });
 const PORT = process.env.PORT || 8000;
@@ -27,6 +28,25 @@ if (process.env.NODE_ENV === "development") {
 
 // mount routes
 app.use("/api/v1/categories", categoryRoute);
+
+// ** 404 unhandling routes middleware
+app.use((req, res, next) => {
+  // const error = new Error(`Can't find this route: ${req.originalUrl}`);
+  // error.statusCode = 404;
+  // next(error);
+  next(new apiError(`Can't find this route: ${req.originalUrl}`, 404));
+});
+
+// global error handler middleware
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode === 200 ? 500 : err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  res.status(statusCode).json({
+    error: err,
+    message: message,
+    stack: process.env.NODE_ENV === "development" ? err.stack : undefined,
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`http://localhost:${PORT}`);
